599e17f65b853260ff1a154cf89aabf20798bc8b
Anatolii <mavtodrug@mail.ru>
2023-11-20 16:57:02 +0500

    commit add to types

--- test_api/views.py
+++ test_api/views.py
@@ -1,24 +1,16 @@
 import re
-
-from django.db.migrations import serializer
-from django.http import JsonResponse
-from django.middleware.csrf import get_token
-from django.shortcuts import render
-from django.views import View
-from django.views.decorators.csrf import csrf_protect
+from urllib.parse import urlencode
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from test_api.models import TempForms
 from test_api.serializers import TempFormsSerializer
 
 
 class TestView(APIView):
 
     def post(self,request):
-
-        #d = request.POST
-        data = check_form(request.GET)
-        #data = TempForms.objects.all()
+        d = urlencode(request.GET)
+        p = dict(map(lambda x: x.split('='), d.split('&')))
+        data = check_form(p)
         serializer = TempFormsSerializer(data, many=True)
         print(serializer.data)
         return Response(serializer.data)
@@ -26,21 +18,25 @@ class TestView(APIView):
 
 def check_type(data):
     test = {'date': r'^\d\d\.\d\d\.\d{4}$',
-            'phone': r'^(\+79\d{9})$',
+            'phone': r'^\+79\s*\d{2}\s*\d{3}\s*\d{2}\s*\d{2}$',
             'email': r'^\S+@\w+.\w{2,4}$',
             }
 
     for types, rex in test.items():
         if re.fullmatch(rex, data):
-            #print(types)
+            print(types)
             return types
 
     return 'text'
 def check_form(data):
+    #print(data)
     res = {}
-    data = data.dict()
+    #data = data.dict()
+    #print(data)
     for k, v in data.items():
-        res[k] = check_type(v)
+        #v = urlencode(v)
         print(v)
+        res[k] = check_type(v)
+        #print(v)
     return res
 
